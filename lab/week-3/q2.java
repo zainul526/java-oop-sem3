a2e400c830d03d72a9eeb4330a135b4b218e96c9
zainul526 <zainulaabideen66@gmail.com>
2026-09-01 23:23:14 +0530

    week 3, Q1

---
+++ lab/week-3/q2.java
@@ -0,0 +1,8 @@
+public class q2{
+    public static void main(String[] args){
+        float num1= 34.98f;
+        float num2 = 781.98f;
+        double product = num1*num2;
+        System.out.println("Product is : "+product);
+    }
+}
