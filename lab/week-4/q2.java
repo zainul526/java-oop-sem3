458bd02e9d716e1780c3fd9e19c6b50b0bc4582d
zainul526 <zainulaabideen66@gmail.com>
2026-09-04 00:16:58 +0530

    Week 4 q.2

--- lab/week-4/q2.java
+++ lab/week-4/q2.java
@@ -1,10 +1,31 @@
 import java.util.*;
+
 public class q2 {
-//2. Write a java program to find the largest number among the three numbers.
-public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter the First Number");
-    double num1 = sc.nextDouble();
-    System.out.println("Enter the  Number");
-}    
+    // 2. Write a java program to find the largest number among the three numbers.
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the First Number");
+        double num1 = sc.nextDouble();
+        System.out.println("Enter the second Number");
+        double num2 = sc.nextDouble();
+        System.out.println("Enter third Number");
+        double num3 = sc.nextDouble();
+        double big = num1;
+
+        if  (num1 == num2 && num2 == num3){
+            System.out.println("All are same");}
+        else {
+            
+           if (num2>big){
+            big = num2;
+           }
+
+           if (num3>big){
+            big=num3;
+           }
+           
+            System.out.println("Biggest is : " + big);
+
+        }
+    }
 }
