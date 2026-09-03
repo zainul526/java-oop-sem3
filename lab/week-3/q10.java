206ee3f81946480352b2fecb87f2773fda3cbbc2
zainul526 <zainulaabideen66@gmail.com>
2026-09-03 23:46:20 +0530

    Week 3 q.10

--- lab/week-3/q10.java
+++ lab/week-3/q10.java
@@ -3,8 +3,9 @@ public class q10 {
     //10. Write a Java program to find the circumference of a circle given its radius.
 public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the radius ");
     double radius = sc.nextDouble();
     double circum = 2*Math.PI*radius;
-    System.out.println("Circumference of the Circle of "+ radius + "radius is : "+circum);
+    System.out.println("Circumference of the Circle of "+ radius + " radius is : "+circum);
 }
 }
