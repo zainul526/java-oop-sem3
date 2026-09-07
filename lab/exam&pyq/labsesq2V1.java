962164e1daea58dc46ec2695cc833627d6363433
zainul526 <zainulaabideen66@gmail.com>
2026-09-07 23:34:10 +0530

    add git ignore

---
+++ lab/exam&pyq/labsesq2V1.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class labsesq2V1 {
+// sum of first and last digit of an integer
+public static void main(String[] args) {
+    // have to complete later or tomorrow
+}    
+}
---
+++ lab/exam&pyq/labsesq3V1.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class labsesq3V1 {
+    // sum of all odd number from 1 to N
+    public static void main(String[] args) {
+        // have to complete
+    }
+}
