8493872f1e88cca61bd48f586c2e744518965f88
zainul526 <zainulaabideen66@gmail.com>
2026-09-07 23:01:32 +0530

    week 6 q1

--- lab/week-6/q1.java
+++ lab/week-6/q1.java
@@ -1,12 +1,11 @@
-public class q1{
-    public static void main(String[] args){
+ class q1{
+    public static void main(String[] arhts){
         for(int i=1; i<=99; i++){
             if(i%2!=0){
                 if(i!=99)
                 {System.out.print(i+", ");}
                 else
                     {System.out.print(i);}
-
             }
         }
     }
