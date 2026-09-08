116178a84e4c4a1b52f474eff4dc944ed998373b
zainul526 <zainulaabideen66@gmail.com>
2026-09-09 00:05:39 +0530

    lab ses q

--- lab/exam&pyq/labsesq2V1.java
+++ lab/exam&pyq/labsesq2V1.java
@@ -1,7 +1,23 @@
 import java.util.*;
+
 public class labsesq2V1 {
-// sum of first and last digit of an integer
-public static void main(String[] args) {
-    // have to complete later or tomorrow
-}    
+    // sum of first and last digit of an integer
+    public static void main(String[] args) {
+        // have to complete later or tomorrow
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int num = sc.nextInt();
+        int lastDig = num % 10;
+        int FirstDig = 0;
+        while (num > 0) {
+            if (num < 10) {
+
+                 FirstDig = num;
+                 break;
+            } else
+                num = num / 10;
+        }
+        System.out.println("The sum of First and  Last digit is: " + (lastDig + FirstDig));
+    }
 }
