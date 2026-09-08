3f3e5a89b0608f00c82949733cc01359706e7707
zainul526 <zainulaabideen66@gmail.com>
2026-09-09 00:18:54 +0530

    lab ses qs

--- lab/exam&pyq/labsesq2V1.java
+++ lab/exam&pyq/labsesq2V1.java
@@ -3,8 +3,6 @@ import java.util.*;
 public class labsesq2V1 {
     // sum of first and last digit of an integer
     public static void main(String[] args) {
-        // have to complete later or tomorrow
-
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number: ");
         int num = sc.nextInt();
--- lab/exam&pyq/labsesq3V1.java
+++ lab/exam&pyq/labsesq3V1.java
@@ -3,6 +3,15 @@ import java.util.*;
 public class labsesq3V1 {
     // sum of all odd number from 1 to N
     public static void main(String[] args) {
-        // have to complete
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Last Number");
+        int N = sc.nextInt();
+        int sum = 0;
+        for(int i = 1; i<=N; i+=2){
+            sum+=i;
+                    
+            
+        }
+        System.out.print("Sum of all odds : "+sum);
     }
 }
