8aa674bdebfd914a33907f4581ead1673af2d0d2
zainul526 <zainulaabideen66@gmail.com>
2026-09-10 00:10:08 +0530

    week 6 q2

--- lab/week-6/q2.java
+++ lab/week-6/q2.java
@@ -1,20 +1,27 @@
 import java.util.*;
+
 public class q2 {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        int i = 1;
-        int count=0;
-        for(i=1;i<=num;i++){
-            if(num%i==0){
-                count++;
-            }
-        }
-        if(count>2){
-            System.out.println("Number is not Prim");
-        }
-        else
-        {System.out.println("Number is prime");}
+    // 2. Write a Java program to check whether a number is prime or not.
+static boolean isPrime(int num){
+    if(num<2){
+        return false;
     }
-    
+    for(int i=2; i<=num/i;i++){
+        if(num%i==0)
+            return false;
+    }
+return true;
+} 
+public static void main(String[] args) {
+ Scanner sc = new Scanner(System.in);
+ System.out.println("Entere number to check");
+ int num = sc.nextInt();
+ if(num == 0)
+ if (isPrime(num)){
+    System.out.println(" Number is Prime ");
+ }
+ else 
+    System.out.println(" Number is not Prime");
+}
+
 }
