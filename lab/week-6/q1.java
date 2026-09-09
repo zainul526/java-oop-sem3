e38a52274a7fd207949fc0297ae29ade3d875b4e
zainul526 <zainulaabideen66@gmail.com>
2026-09-09 22:46:16 +0530

    week 6 q1

--- lab/week-6/q1.java
+++ lab/week-6/q1.java
@@ -1,7 +1,9 @@
  class q1{
-    public static void main(String[] arhts){
-        for(int i=1; i<=99; i++){
-            if(i%2!=0){
+    //1. Write a Java program to print the odd numbers from 1 to 99.
+
+    public static void main(String[] args){
+        for(int i=1; i<=99; i+=2){
+            {
                 if(i!=99)
                 {System.out.print(i+", ");}
                 else
