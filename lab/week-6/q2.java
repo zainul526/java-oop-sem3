f88d0ae8d7fdd4bea52aa9a0152326ef4238736a
zainul526 <zainulaabideen66@gmail.com>
2026-09-11 23:02:30 +0530

    week 6 q2

--- lab/week-6/q2.java
+++ lab/week-6/q2.java
@@ -3,9 +3,7 @@ import java.util.*;
 public class q2 {
     // 2. Write a Java program to check whether a number is prime or not.
     static boolean isPrime(int num) {
-        if (num < 2) {
-            return false;
-        }
+        
         for (int i = 2; i <= num / i; i++) {
             if (num % i == 0)
                 return false;
@@ -14,14 +12,23 @@ public class q2 {
     }
 
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         System.out.println("Entere number to check");
         int num = sc.nextInt();
-        if (num == 0)
-            if (isPrime(num)) {
+        if (num < 1){
+            System.out.println("Not Defined for Negatives and Zeros");
+        }
+        else if(num ==1){
+            System.out.println("Neither prime nor COmposite");
+        }
+        else
+            {
+                if (isPrime(num)) {
                 System.out.println(" Number is Prime ");
             } else
                 System.out.println(" Number is not Prime");
-    }
+            }
+            }
 
 }
--- lab/week-6/q3.java
+++ lab/week-6/q3.java
@@ -1,4 +1,6 @@
 public class q3 {
+    //3. Write a Java program to swap the first and last elements of an array.
+
     public static void main(String[] args){
         
     }
