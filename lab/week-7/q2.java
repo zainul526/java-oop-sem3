b3154757a31b30bd9f97382e7bf6e46064efb8e0
zainul526 <zainulaabideen66@gmail.com>
2026-09-14 22:21:37 +0530

    week 7 q2

---
+++ lab/week-7/q2.java
@@ -0,0 +1,31 @@
+public class q2 {
+
+    static void sortAscending(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {45, 12, 78, 3, 21};
+
+        sortAscending(arr);
+
+        System.out.println("Array in ascending order:");
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
