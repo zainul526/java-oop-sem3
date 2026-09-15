27d012daf9042652c80fdca714bfd15685dbacef
zainul526 <zainulaabideen66@gmail.com>
2026-09-15 23:32:04 +0530

    classques

---
+++ classQues/q1.java
@@ -0,0 +1,10 @@
+public class q1 {
+    ////java prog to print Hello World on screen
+    public static void hello(){
+        System.out.println("Hello World");
+    }
+    public static void main(String[] args){
+        hello();
+    }
+    
+}
