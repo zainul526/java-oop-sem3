5c44497c9ebe6cf6e784bfb1e5a87c910aa6f03c
zainul526 <zainulaabideen66@gmail.com>
2026-09-18 23:01:08 +0530

    week 3 q3

--- lab/week-3/q3.java
+++ lab/week-3/q3.java
@@ -3,7 +3,7 @@
 public class q3{
     public static void main(String [] args){
         double num1 = 17.67;
-        double cube = Math.pow(num1,3);
+        double cube = num1*num1*num1;
         System.out.println("Cube of "+ num1 +" is: "+ cube );
     }
 }
